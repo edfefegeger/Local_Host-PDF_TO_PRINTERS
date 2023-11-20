f8d45eab1d3d5a2fa8737cbdb2943fb36bd7eda2
MYXAMOR338 <silova155@gmail.com>
2023-11-20 17:45:15 +0300

    Second almost work but without result

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import win32print
+import win32con
 import requests
 
 app = Flask(__name__)
@@ -10,45 +11,58 @@ def index():
     return render_template('index.html')
 
 @app.route('/print', methods=['GET'])
-@app.route('/print', methods=['GET'])
-@app.route('/print', methods=['GET'])
 def print_file():
     file_url = request.args.get('file_url')
     paper_size = request.args.get('paper_size')
     printer_name = request.args.get('printer_name')
 
-    # Скачиваем файл
+    # Download the file
     response = requests.get(file_url)
     with open('temp_file.pdf', 'wb') as f:
         f.write(response.content)
 
-    # Устанавливаем принтер по умолчанию
-    win32print.SetDefaultPrinter(printer_name)
+    try:
+        # Open the specified printer
+        hPrinter = win32print.OpenPrinter(printer_name)
+        
+        # Get the default printer settings
+        default_printer_settings = win32print.GetPrinter(hPrinter, 2)['pDevMode']
 
-    # Получаем информацию о принтере по умолчанию
-    default_printer = win32print.GetDefaultPrinter()
-    # Получаем информацию о принтере
-    printer_info = win32print.GetPrinter(win32print.OpenPrinter(default_printer))
+        # Print some debugging information
+        print("Printer Name:", printer_name)
+        print("Supported Paper Sizes:", default_printer_settings.PaperSize)
+        
+        # Set the paper size
+        default_printer_settings.PaperSize = getattr(win32print, 'DMPAPER_' + paper_size, default_printer_settings.PaperSize)
 
-    # Задаем параметры печати
-    print_data = {
-        'Title': 'Print Job',
-        'PrintToFile': False,
-        'PaperSize': getattr(win32print, paper_size),
-        'PrintQuality': getattr(win32print, 'DPI_HIGH'),
-    }
+        # Print additional debugging information
+        print("Selected Paper Size:", default_printer_settings.PaperSize)
 
-    # Печатаем файл
-    win32print.StartDocPrinter(printer_info['hPrinter'], 1, (os.path.basename('temp_file.pdf'), None, 'RAW'))
-    win32print.WritePrinter(printer_info['hPrinter'], response.content)
-    win32print.EndDocPrinter(printer_info['hPrinter'])
-    win32print.ClosePrinter(printer_info['hPrinter'])
+        # Set up print data
+        print_data = {
+            'Title': 'Print Job',
+            'PrintToFile': False,
+            'pDevMode': default_printer_settings,
+        }
 
-    # Удаляем временный файл
+        # Print additional debugging information
+        print("Print Data:", print_data)
+
+        # Start printing
+        hJob = win32print.StartDocPrinter(hPrinter, 1, ('temp_file.pdf', None, 'RAW'))
+        win32print.StartPagePrinter(hPrinter)
+        win32print.WritePrinter(hPrinter, response.content)
+        win32print.EndPagePrinter(hPrinter)
+        win32print.EndDocPrinter(hPrinter)
+
+    finally:
+        # Close the printer handle
+        win32print.ClosePrinter(hPrinter)
+
+    # Remove the temporary file
     os.remove('temp_file.pdf')
 
     return jsonify({'status': 'success'})
-
 
 if __name__ == '__main__':
     app.run(debug=True)
