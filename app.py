5751e8b9a244c477146b9f8b4088769240616fc4
MYXAMOR338 <silova155@gmail.com>
2023-11-21 14:03:57 +0300

    comments on russian language added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import traceback  # Добавленный импорт
 import hashlib
 import urllib.parse
 import webbrowser
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,16 +15,17 @@ def index():
 
 @app.route('/print', methods=['GET'])
 def print_file():
+    # Получаем URL, размер бумаги и имя принтера из параметров запроса
     file_url = request.args.get('file_url')
     paper_size = request.args.get('paper_size')
     printer_name = request.args.get('printer_name')
 
-    # Sanitize URL and create a unique name for the temporary file based on URL and paper size
+    # Очищаем URL и создаем уникальное имя временного файла на основе URL и размера бумаги
     sanitized_url = hashlib.sha256(file_url.encode()).hexdigest()
-    sanitized_url = urllib.parse.quote_plus(sanitized_url)  # Replace problematic characters with URL-safe characters
+    sanitized_url = urllib.parse.quote_plus(sanitized_url)  # Заменяем проблемные символы на безопасные для URL
     unique_filename = f'temp_file_{sanitized_url}_size_{paper_size}.pdf'
 
-    # Download the file
+    # Загружаем файл
     response = requests.get(file_url)
     with open(unique_filename, 'wb') as f:
         f.write(response.content)
@@ -31,54 +33,54 @@ def print_file():
     success = False
 
     try:
-        # Open the specified printer
+        # Открываем указанный принтер
         hPrinter = win32print.OpenPrinter(printer_name)
         
-        # Get the default printer settings
+        # Получаем параметры по умолчанию принтера
         default_printer_settings = win32print.GetPrinter(hPrinter, 2)['pDevMode']
 
-        # Set the paper size
+        # Устанавливаем размер бумаги
         default_printer_settings.PaperSize = getattr(win32print, paper_size, default_printer_settings.PaperSize)
 
-        # Set up print data
+        # Подготавливаем данные для печати
         print_data = {
             'Title': 'Print Job',
             'PrintToFile': False,
             'pDevMode': default_printer_settings,
         }
 
-        # Start printing
+        # Начинаем печать
         hJob = win32print.StartDocPrinter(hPrinter, 1, (unique_filename, None, 'RAW'))
         win32print.StartPagePrinter(hPrinter)
 
-        # Read the content of the PDF file and write it to the printer
+        # Читаем содержимое файла PDF и записываем его в принтер
         with open(unique_filename, 'rb') as pdf_file:
             win32print.WritePrinter(hPrinter, pdf_file.read())
 
         win32print.EndPagePrinter(hPrinter)
         win32print.EndDocPrinter(hPrinter)
 
-        # If code reaches here, consider printing successful
+        # Если код дошел до этого момента, считаем печать успешной
         success = True
 
     except Exception as e:
-        print(f"Error printing: {e}")
+        print(f"Ошибка при печати: {e}")
         traceback.print_exc()
 
     finally:
-        # Close the printer handle
+        # Закрываем дескриптор принтера
         win32print.ClosePrinter(hPrinter)
 
-    # Remove the temporary file
+    # Удаляем временный файл
     os.remove(unique_filename)
 
-    # Check if printing was successful
+    # Проверяем успешность печати
     if success:
         result_message = 'Успешная печать'
     else:
         result_message = 'Ошибка при печати'
 
-    # Вернуть HTML-страницу с обновленным содержимым div и информацией о формате бумаги
+    # Возвращаем HTML-страницу с обновленным содержимым div и информацией о формате бумаги
     return render_template('index.html', result_message=result_message, paper_size=paper_size)
 
 if __name__ == '__main__':
