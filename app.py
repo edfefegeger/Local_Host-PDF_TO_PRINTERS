d3da43e9a166ceaf64f94f3cec2f1c0de8b0ff63
MYXAMOR338 <silova155@gmail.com>
2023-11-26 18:34:41 +0300

    Whole text printing

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 import traceback
 from flask import Flask, render_template, request
 import os
-import win32print
+import win32printing
 import requests
 import hashlib
 import urllib.parse
-import threading
-import time
-from PyPDF2 import PdfReader, PdfWriter
-import pywintypes
+import tempfile
+import fitz  # PyMuPDF
 import win32timezone
-
-from flask import Flask, render_template, request
-import win32printing
 
 app = Flask(__name__)
 
@@ -37,12 +32,26 @@ def print_file():
     success = False
 
     try:
-        with win32printing.Printer() as printer:
+        # Open the printer
+        with win32printing.Printer(printer_name) as printer:
+            # Start a print job
             printer.text(f"Printing file: {file_url}")
+
+            # Send the PDF content to the printer
+            with open(unique_filename, 'rb') as pdf_file:
+                # Using PyMuPDF to read the PDF content
+                pdf_document = fitz.open(pdf_file)
+                
+                for page_number in range(pdf_document.page_count):
+                    page = pdf_document.load_page(page_number)
+                    page_text = page.get_text()
+                    
+                    # Send the text to the printer
+                    printer.text(page_text)
 
         success = True
 
-    except win32printing.error as e:
+    except Exception as e:
         print(f"Error while printing: {e}")
         traceback.print_exc()
 
