aca5927ff6849ef8b30c2a7319356417eb4de7e7
MYXAMOR338 <silova155@gmail.com>
2023-11-27 10:09:28 +0300

    add dropping list

--- app.py
+++ app.py
@@ -3,13 +3,23 @@ import os
 import requests
 import subprocess
 from flask import Flask, render_template, request
+from flask import jsonify
+import win32print
 
 app = Flask(__name__)
+
+def set_sumatra_paper_size(settings_path, paper_size):
+    with open(settings_path, 'a') as settings_file:
+        settings_file.write(f'CustomPageSize = {paper_size}\n')
 
 def print_pdf(file_path, printer_name):
     sumatra_path = r'SumatraPDF\SumatraPDF.exe'
     subprocess.run([sumatra_path, '-print-to', printer_name, file_path])
 
+@app.route('/get_printers')
+def get_printers():
+    printers = [printer[2] for printer in win32print.EnumPrinters(2)]
+    return jsonify({'printers': printers})
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -28,15 +38,19 @@ def print_file():
     with open(unique_filename, 'wb') as f:
         f.write(response.content)
 
+    # Указать путь к файлу SumatraPDF-settings.txt
+    sumatra_settings_path = os.path.join(os.path.dirname('SumatraPDF\SumatraPDF.exe'), 'SumatraPDF-settings.txt')
+
+
     success = False
 
     try:
+        set_sumatra_paper_size(sumatra_settings_path, paper_size)
         print_pdf(unique_filename, printer_name)
         success = True
     except Exception as e:
         print(f"Error while printing: {e}")
     finally:
-        # Удаление временного файла после завершения
         os.remove(unique_filename)
 
     return render_template('index.html', result_message="Printing initiated", paper_size=paper_size, success=success)
